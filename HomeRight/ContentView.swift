0492a39f482269a74bf252d95363b4db97dcafcc
Marish <mbzebradev@gmail.com>
2025-12-05 18:03:17 -0700

    Initial Commit

---
+++ HomeRight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeRight
+//
+//  Created by Mariswaran Balasubramanian on 12/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeRight/HomeRightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeRightApp.swift
+//  HomeRight
+//
+//  Created by Mariswaran Balasubramanian on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeRightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
